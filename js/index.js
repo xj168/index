43b406881bf713d417f73609235e34a9a91aba85
xj168 <65447815+xj168@users.noreply.github.com>
2024-05-09 22:35:40 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -30,6 +30,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const countdownInterval = setInterval(function() { 
         updateCountdown('LOVE', LOVEDate); 
         updateCountdown('MARRIAGE', MARRIAGEDate); 
-        updateCountdown('CERTIFICATE',CERTIFICATEData); 
+        updateCountdown('CERTIFICATE', CERTIFICATEData); 
     }, 1000);
 });
