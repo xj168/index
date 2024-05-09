da8bb236e0ae9e5188b65e11960c326469c017f9
xj168 <65447815+xj168@users.noreply.github.com>
2024-05-09 22:34:40 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // 倒计时目标时间，格式为YYYY-MM-DD HH:MM:SS
     const LOVEDate = new Date("2017-02-11 00:00:00").getTime();
     const MARRIAGEDate = new Date("2023-01-26 12:06:00").getTime();
-    const CERTIFICTEDate = new Date("2022-12-22 14:08:00").getTime();
+    const CERTIFICATEData = new Date("2022-12-22 14:08:00").getTime();
 
     function updateCountdown(elementId, targetDate) {
         const now = new Date().getTime();
@@ -30,6 +30,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const countdownInterval = setInterval(function() { 
         updateCountdown('LOVE', LOVEDate); 
         updateCountdown('MARRIAGE', MARRIAGEDate); 
-        updateCountdown('CERTIFICTE',CERTIFICTEDate); 
+        updateCountdown('CERTIFICATE',CERTIFICATEData); 
     }, 1000);
 });
